937dd4ab2e46c68cefa707c3630f4c85299bdd03
leopantsulaia <leopantsulaia@gmail.com>
2025-05-13 04:24:55 +0400

    Caution added! ready to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -26,24 +26,48 @@ const App = () => {
   if (isLoading) return <div className="loading">Loading...</div>;
 
   return (
-    <div className="container">
-      {currentUser ? (
-        <>
-          <List />
+    <>
+      <p style={{ backgroundColor: "red" }}>
+        some data is already created to avoid payments. Whhile filling the login
+        fields, refresh the website {"<3"}
+      </p>
+      <p style={{ backgroundColor: "red" }}>
+        sign in example 1: Username: Levan , Email: levan@gmail.com , password:
+        Levan123
+      </p>
+      <p style={{ backgroundColor: "red" }}>
+        sign in example 2: Username: Pantsu , Email: pantsu@gmail.com ,
+        password: Pantsu123
+      </p>
+      <p style={{ backgroundColor: "red" }}>
+        sign in example 3: Username: localhost , Email: localhost@localhost.com
+        , password: localhost
+      </p>
+      <p style={{ backgroundColor: "red" }}>
+        If you still want to create your own account, remember upload the dummy
+        image first, note: pls upload small image and inform me if you got
+        suggestions
+      </p>
+      <div className="container">
+        {currentUser ? (
+          <>
+            <List />
 
-          {chatId && <Chat />}
-          {chatId && <Detail />}
+            {chatId && <Chat />}
+            {chatId && <Detail />}
 
-
-          <button style={{ backgroundColor: "red", height: "50px", width: "100px" }} onClick={() => auth.signOut()}>
-           Logout
-          </button>
-        </>
-      ) : (
-        <Login />
-      )}
-      <Notification />
-    </div>
+            <button
+              style={{ backgroundColor: "red", height: "50px", width: "100px" }}
+              onClick={() => auth.signOut()}>
+              Logout
+            </button>
+          </>
+        ) : (
+          <Login />
+        )}
+        <Notification />
+      </div>
+    </>
   );
 };
 
